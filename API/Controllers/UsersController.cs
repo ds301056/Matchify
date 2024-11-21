017fa491b8ed1dbd4d5d48cace97195269807924
ds301056 <139257740+ds301056@users.noreply.github.com>
2024-11-21 15:22:43 -0500

    asynchronous

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 // controller for users
@@ -11,20 +12,20 @@ namespace API.Controllers;
 public class UsersController(DataContext context) : ControllerBase
 {
     [HttpGet] // method to return response to client 
-  public ActionResult<IEnumerable<AppUser>> GetUsers()
+  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
   {
     // return list of users 
-    var users = context.Users.ToList();
+    var users = await context.Users.ToListAsync();
 
     return users;
 
   }
 
     [HttpGet("{id:int}")] // api/users/{id}
-  public ActionResult<AppUser> GetUser(int id)
+  public async Task<ActionResult<AppUser>> GetUser(int id)
   {
     // return list of users 
-    var user = context.Users.Find(id); // get user by id 
+    var user = await context.Users.FindAsync(id); // get user by id 
 
     if (user == null) return NotFound(); // if user not found return 404
 
