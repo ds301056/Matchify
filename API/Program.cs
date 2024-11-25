8c3bffdf8a4cd510c0968f5a4d817d1ce093627c
ds301056 <139257740+ds301056@users.noreply.github.com>
2024-11-25 15:28:04 -0500

    https_certs_forLocalHost

--- API/Program.cs
+++ API/Program.cs
@@ -8,29 +8,21 @@ builder.Services.AddControllers();
 // Add detailed error handling and logging
 builder.Services.AddDbContext<DataContext>(opt => 
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"),
-        options => options.MigrationsAssembly(typeof(DataContext).Assembly.FullName));
-    opt.EnableSensitiveDataLogging();
-    opt.EnableDetailedErrors();
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+
+//ADD CORS POLICY
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
-// Add database initialization
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<DataContext>();
-        context.Database.Migrate();
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred during database migration");
-    }
-}
+
+
+// configure the HTTP request pipeline
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 
 app.MapControllers();
 app.Run();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { HttpClient } from '@angular/common/http'
+import { NgFor } from '@angular/common'
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,16 @@ import { RouterOutlet } from '@angular/router'
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient)
   title = 'Matchify'
+  users: any
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    })
+  }
 }
